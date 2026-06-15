008c9d927ef0fecf6f728068f7d8731205f2e34f
DuvalK78 <duval.kerian18@ik.me>
2026-06-15 23:58:01 +0200

    Create data.js

---
+++ assets/js/data.js
@@ -0,0 +1,27 @@
+const popupMessages = [
+
+"存在検出",
+"接続",
+"記録中",
+"監視中",
+"認証失敗",
+"区画_08",
+"区画_14",
+"KSRK://NODE_17",
+"KSRK://NODE_91",
+"NODE_44",
+"NODE_88",
+"ARCHIVE LOST",
+"ARCHIVE LOCKED",
+"DATA FRAGMENT",
+"UNREGISTERED",
+"SIGNAL LOST",
+"ERROR_77",
+"ERROR",
+"CORRUPTED",
+"???",
+"████████",
+"記録破損",
+"警告"
+
+];
